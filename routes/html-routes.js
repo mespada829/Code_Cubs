89a4d410185ed2fe825c7ecca2520767f7cc562f
Marita Espada <MaritaEspada@Maritas-MacBook-Air.local>
2017-07-29 13:20:32 -0400

    Adding Welcome Page

---
+++ routes/html-routes.js
@@ -0,0 +1,12 @@
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
