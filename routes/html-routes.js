0954fa266b984aa510e01001a8c8fe002978fc0b
claeys Emolo <claeysemolo@claeyss-MacBook-Pro.local>
2017-08-01 18:24:45 -0400

    added route to /search to render index.handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
-=======
+
 // *********************************************************************************
 // html-routes.js - this file offers a set of routes for sending users to the various html pages
 // *********************************************************************************
 
->>>>>>> 52070143a7f6b4a89d89bbd988e722e2d87d7f01
 // Dependencies
 // =============================================================
 var path = require("path");
@@ -13,12 +11,11 @@ var path = require("path");
 // =============================================================
 module.exports = function(app) {
 
-<<<<<<< HEAD
 
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-=======
+
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
@@ -34,6 +31,9 @@ module.exports = function(app) {
   app.get("/blog", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/blog.html"));
   });
+  app.get("/search", function(req, res) {
+    res.render("index");
+  });
 
 };
->>>>>>> 52070143a7f6b4a89d89bbd988e722e2d87d7f01
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require ('express-handlebars');
 
 // Sets up the Express App
 // =============================================================
@@ -17,6 +18,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Handlebars
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
 
 // Static directory
 app.use(express.static("public"));
@@ -33,3 +38,4 @@ db.sequelize.sync({ force: true }).then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
